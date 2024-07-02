786406d2562e45ab7bd46f62306fd36655474ce5
Famondir <43599267+Famondir@users.noreply.github.com>
2024-07-02 12:02:58 +0200

    Added lor data and inhabitants information sorted by lor

--- code/testcode_simon.R
+++ code/testcode_simon.R
@@ -1,13 +1,46 @@
 source("code/setup.R")
 
 elect_units <- sf::st_read(dsn = "data/Wahlbezirke") %>% 
-  st_make_valid(tol = 0.00001) %>% st_transform(4326)
+  st_make_valid(tol = 0.00001) %>% st_transform(4326) %>% 
+  mutate(BEZNAME = str_replace_all(BEZNAME, "\xf6", "ö"))
 table(st_is_valid(elect_units))
 crs <- st_crs(elect_units)
 
 # opnv <- sf::st_read(dsn = "data/opnv/") %>% 
 #   st_make_valid(tol = 0.00001) %>% st_transform(4326)
 # table(st_is_valid(opnv))
+
+bezirke_name_id <- tribble(
+  ~BEZ_NAME, ~BEZ_ID,
+  "Mitte", "01",
+  "Friedrichshain-Kreuzberg", "02",
+  "Pankow", "03",
+  "Charlottenburg-Wilmersdorf", "04",
+  "Spandau", "05",
+  "Steglitz-Zehlendorf", "06",
+  "Tempelhof-Schöneberg", "07",
+  "Neukölln", "08",
+  "Treptow-Köpenick", "09",
+  "Marzahn-Hellersdorf", "10",
+  "Lichtenberg", "11",
+  "Reinickendorf", "12",
+  )
+
+lor_inhabitants <- read_csv2("data/lor/einwohner_lor_2020-12.csv", col_types = "c") %>% 
+  select(-c(ZEIT, BEZ, PGR, BZR, PLR, STADTRAUM))
+lor_foreigner <- read_csv2("data/lor/einwohner_foreign_lor_2020-12.csv", col_types = "c") %>% 
+  select(-c(ZEIT, BEZ, PGR, BZR, PLR, STADTRAUM))
+lor_migration_bg <- read_csv2("data/lor/einwohner_migrationbackground_lor_2020-12.csv", col_types = "c") %>% 
+  select(-c(ZEIT, BEZ, PGR, BZR, PLR, STADTRAUM))
+
+lor <- sf::st_read(dsn = "data/lor/Planung") %>% 
+  st_make_valid(tol = 0.00001) %>% st_transform(4326) %>% 
+  mutate(
+    BEZ_ID = str_sub(PLR_ID, 1, 2),
+    PGR_ID = str_sub(PLR_ID, 1, 4),
+    BZR_ID = str_sub(PLR_ID, 1, 6)
+    ) %>% 
+  left_join(bezirke_name_id, by = c("BEZ_ID" = "BEZ_ID"))
 
 airbnb <- rio::import("data/airbnb/March_2024/listings.csv") %>% 
   as_tibble()
@@ -34,6 +67,21 @@ elect_units %>%
   geom_sf(
     mapping = aes(geometry = geometry, fill = BEZNAME)
     ) +
+  geom_sf(
+    data = points,
+    aes(geometry = geometry),
+    size = 1,
+    color = "black",
+    alpha = .3) +
+  # geom_sf(
+  #   data = opnv, mapping = aes(geometry = geometry), color = "red") +
+  theme_bw()
+
+lor %>% 
+  ggplot() + 
+  geom_sf(
+    mapping = aes(geometry = geometry, fill = BEZ_ID)
+  ) +
   geom_sf(
     data = points,
     aes(geometry = geometry),
